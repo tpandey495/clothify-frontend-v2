4acb3826730c3b70bb2216760da64381201e6652
tpandey495 <tpandey495@gmail.com>
2023-09-27 22:00:21 +0530

    made link accessible

--- src/component/Navbar/MiddleNav.js
+++ src/component/Navbar/MiddleNav.js
@@ -3,7 +3,8 @@ import "styles/middlenavbar.css";
 import line from "assets/images/line.png";
 import LastNav from "./LastNav";
 import useWindowWidth from "utils/windowWidth";
-import { UseSelector, useSelector } from "react-redux";
+import {useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const MiddleNav = () => {
   const count = useSelector((state)=>state?.cart?.cartsize);
 
@@ -11,10 +12,10 @@ const MiddleNav = () => {
   return (
     <div className="header">
       <div className="middle-navbar">
-        <div className="middle-title">ShopKart</div>
+        <div className="middle-title"><Link to="/">ShopKart</Link></div>
         {windowWidth > 600 ? (
           <ul className="middle-items">
-            <li>Cart({count})</li>
+            <li><Link to="/cart">Cart({count})</Link></li>
           </ul>
         ):(
           <LastNav />
